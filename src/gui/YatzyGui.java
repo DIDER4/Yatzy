ba2adb2b4787474a1d02c4bdfb1e4f0c49dc1198
BuildTools <unconfigured@null.spigotmc.org>
2024-10-29 11:22:41 +0100

    Gui update

--- src/gui/YatzyGui.java
+++ src/gui/YatzyGui.java
@@ -12,7 +12,7 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class YatzyGui extends Application {
-    private TextField[] scoreFields;
+    private TextField[] scoreFelt;
 
     @Override
     public void start(Stage primaryStage) {
@@ -22,7 +22,7 @@ public class YatzyGui extends Application {
         terningeGrid.setHgap(10);
         terningeGrid.setVgap(10);
 
-        // Terninger og hold checkbokse
+        // Terningerne og hold checkbokse
         Label[] terningeLabels = new Label[5];
         CheckBox[] holdCheckBoxes = new CheckBox[5];
         for (int i = 0; i < 5; i++) {
@@ -39,7 +39,7 @@ public class YatzyGui extends Application {
         Label rultilbage = new Label("Antal kast tilbage: ");
         Label antalRulTilbage = new Label("0");
 
-        VBox terningeBox = new VBox(10, terningeGrid, rulKnap, rultilbage);
+        VBox terningeBox = new VBox(10, terningeGrid, rulKnap, rultilbage, antalRulTilbage);
         terningeBox.setPadding(new Insets(10));
 
         // GridPane for scorekortet
@@ -48,18 +48,19 @@ public class YatzyGui extends Application {
         scoreGrid.setHgap(10);
         scoreGrid.setVgap(10);
 
-        String[] categories = {"1'ere", "2'ere", "3'ere", "4'ere", "5'ere", "6'ere", "Sum", "Bonus", "Et par", "To par", "3 ens", "4 ens", "Lille straight", "Store straight", "Fuldt hus", "Chance", "Yatzy"};
-        scoreFields = new TextField[categories.length];
-        for (int i = 0; i < categories.length; i++) {
-            scoreGrid.add(new Label(categories[i]), 0, i);
-            scoreFields[i] = new TextField();
-            scoreGrid.add(scoreFields[i], 1, i);
+        String[] pointMuligheder = {"1'ere", "2'ere", "3'ere", "4'ere", "5'ere", "6'ere", "Sum", "Bonus", "Et par", "To par", "3 ens", "4 ens", "Lille straight", "Store straight", "Fuldt hus", "Chance", "Yatzy"};
+        scoreFelt = new TextField[pointMuligheder.length];
+        for (int i = 0; i < pointMuligheder.length; i++) {
+            scoreGrid.add(new Label(pointMuligheder[i]), 0, i);
+            scoreFelt[i] = new TextField();
+            scoreGrid.add(scoreFelt[i], 1, i);
         }
 
+
         Label totalLabel = new Label("Total:");
-        TextField totalField = new TextField();
-        scoreGrid.add(totalLabel, 0, categories.length);
-        scoreGrid.add(totalField, 1, categories.length);
+        TextField totalFelt = new TextField();
+        scoreGrid.add(totalLabel, 0, pointMuligheder.length+1);
+        scoreGrid.add(totalFelt, 1, pointMuligheder.length+1);
 
         VBox root = new VBox(20, terningeBox, scoreGrid);
         Scene scene = new Scene(root, 400, 900);
@@ -67,9 +68,5 @@ public class YatzyGui extends Application {
         primaryStage.setTitle("Yatzy App");
         primaryStage.setScene(scene);
         primaryStage.show();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
     }
 }
